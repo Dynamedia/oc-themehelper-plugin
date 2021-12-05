22a8efdf9bc61464b9d1e60af53119e9b724ba6e
Rob Ballantyne <rob@dynamedia.uk>
2021-12-05 08:54:07 +0000

    first commit

---
+++ Plugin.php
@@ -0,0 +1,85 @@
+<?php namespace Dynamedia\Themehelper;
+
+use Backend;
+use System\Classes\PluginBase;
+
+/**
+ * themehelper Plugin Information File
+ */
+class Plugin extends PluginBase
+{
+    /**
+     * Returns information about this plugin.
+     *
+     * @return array
+     */
+    public function pluginDetails()
+    {
+        return [
+            'name'        => 'themehelper',
+            'description' => 'Provide help to the active theme',
+            'author'      => 'dynamedia',
+            'icon'        => 'icon-leaf'
+        ];
+    }
+
+    /**
+     * Register method, called when the plugin is first registered.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+
+    /**
+     * Boot method, called right before the request route.
+     *
+     * @return array
+     */
+    public function boot()
+    {
+        // Add any columns you want as nested forms to this array
+        $jsonable_fields = [
+          'test_submenu',
+        ];
+        \Cms\Models\ThemeData::extend(function ($model) use ($jsonable_fields) {
+            $model->addJsonable($jsonable_fields);
+        });
+    }
+
+    /**
+     * Registers any front-end components implemented in this plugin.
+     *
+     * @return array
+     */
+    public function registerComponents()
+    {
+        return []; // Remove this line to activate
+
+        return [
+            'Dynamedia\Themehelper\Components\MyComponent' => 'myComponent',
+        ];
+    }
+
+    /**
+     * Registers any back-end permissions used by this plugin.
+     *
+     * @return array
+     */
+    public function registerPermissions()
+    {
+        return []; // Remove this line to activate
+    }
+
+    /**
+     * Registers back-end navigation items for this plugin.
+     *
+     * @return array
+     */
+    public function registerNavigation()
+    {
+        return []; // Remove this line to activate
+    }
+}
